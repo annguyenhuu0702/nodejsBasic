6a6bad6c58b358bef3b04ffdb487a32e272326ea
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-21 00:01:09 +0700

    connect database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,20 @@
+import connection from "./../configs/connectDB";
+
 let getHomepage = (req, res) => {
-  return res.render("index.ejs");
+  let data = [];
+  connection.query("SELECT * FROM `users`", function (err, results, fields) {
+    results.map((row) => {
+      data.push({
+        id: row.id,
+        email: row.email,
+        addres: row.addres,
+        firstName: row.firstName,
+        lastName: row.lastName,
+      });
+    });
+
+    return res.render("index.ejs", { dataUser: JSON.stringify(data) });
+  });
 };
 
 module.exports = {
