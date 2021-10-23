0d737d5fa68d90b5f70cb6f6b193a9697d468b44
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-23 23:03:40 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -13,7 +13,6 @@ let getDetailpage = async (req, res) => {
 };
 
 let creteNewUser = async (req, res) => {
-  console.log("data", req.body);
   let { firstName, lastName, email, address } = req.body;
 
   await pool.execute(
@@ -23,8 +22,32 @@ let creteNewUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("delete from users where id = ?", [userId]);
+  return res.redirect("/");
+};
+
+let getEditPage = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await pool.execute("select * from users where id = ?", [id]);
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+
+let postUpdateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    "update users set firstName = ?,lastName = ? ,email = ?,address =? where id = ?",
+    [firstName, lastName, email, address, id]
+  );
+  return res.redirect("/");
+};
+
 module.exports = {
   getHomepage,
   getDetailpage,
   creteNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -5,11 +5,11 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
-
   router.get("/detail/user/:id", homeController.getDetailpage);
-
   router.post("/create-new-user", homeController.creteNewUser);
-
+  router.post("/delete-user", homeController.deleteUser);
+  router.get("/edit-user/:id", homeController.getEditPage);
+  router.post("/update-user", homeController.postUpdateUser);
   router.get("/abc", (req, res) => {
     res.send("helo anh em học lập trình");
   });
