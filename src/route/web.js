5089983786df0a88fe851dc124264bd0f1b0cb4b
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-25 18:09:16 +0700

    upload multiple file

--- src/route/web.js
+++ src/route/web.js
@@ -30,6 +30,10 @@ const imageFilter = function (req, file, cb) {
 };
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
+let uploadMultipleFiles = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+}).array("multiple_images", 3);
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
@@ -43,6 +47,26 @@ const initWebRoute = (app) => {
     "/upload-profile-pic",
     upload.single("profile_pic"),
     homeController.handleUploadFile
+  );
+  router.post(
+    "/upload-multiple-images",
+    (req, res, next) => {
+      uploadMultipleFiles(req, res, (err) => {
+        if (
+          err instanceof multer.MulterError &&
+          err.code === "LIMIT_UNEXPECTED_FILE"
+        ) {
+          // handle multer file limit error here
+          res.send("LIMIT_UNEXPECTED_FILE");
+        } else if (err) {
+          res.send(err);
+        } else {
+          // make sure to call next() if all was well
+          next();
+        }
+      });
+    },
+    homeController.handleUploadMultipleFiles
   );
   router.get("/abc", (req, res) => {
     res.send("helo anh em học lập trình");
