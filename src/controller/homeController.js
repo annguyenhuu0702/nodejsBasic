998f6c9fe86473b3f49e3a9f8c7cc47e6cc87645
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-25 14:37:08 +0700

    upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import pool from "./../configs/connectDB";
+import multer from "multer";
 
 let getHomepage = async (req, res) => {
   const [rows, fields] = await pool.execute("SELECT * FROM users");
@@ -43,6 +44,61 @@ let postUpdateUser = async (req, res) => {
   return res.redirect("/");
 };
 
+let getUpLoadFilePage = async (req, res) => {
+  return res.render("uploadFile.ejs");
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, appRoot + "/src/public/image/");
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+let handleUploadFile = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+  let upload = multer({
+    storage: storage,
+    fileFilter: imageFilter,
+  }).single("profile_pic");
+
+  upload(req, res, function (err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("Please select an image to upload");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+
+    // Display uploaded image for user validation
+    res.send(
+      `You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+    );
+  });
+};
+
 module.exports = {
   getHomepage,
   getDetailpage,
@@ -50,4 +106,6 @@ module.exports = {
   deleteUser,
   getEditPage,
   postUpdateUser,
+  getUpLoadFilePage,
+  handleUploadFile,
 };
