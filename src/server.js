516328812e88cbdd7f27084654f82169ce7af2f4
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-25 18:59:36 +0700

    midleware basic

--- src/server.js
+++ src/server.js
@@ -2,10 +2,18 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web";
 import initAPIRoute from "./route/api";
+
 require("dotenv").config();
+var morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  console.log(">>>run into my middleware");
+  next();
+});
+app.use(morgan("combined"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +26,11 @@ initWebRoute(app);
 
 // init api route
 initAPIRoute(app);
+
+// handle 404 not found
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
